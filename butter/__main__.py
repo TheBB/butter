88d74faee024673e4e5d600ab59eaec3aaa66ede
Eivind Fonn <evfonn@gmail.com>
2017-10-21 16:36:32 +0200

    Improved DB, plugin system and configuration

--- butter/__main__.py
+++ butter/__main__.py
@@ -1,30 +1,34 @@
 import click
-import imagehash
-import inflect
-from itertools import combinations
-import multiprocessing
-from os.path import basename, join, splitext
-from PIL import Image
-import requests
-import shutil
-from tempfile import TemporaryDirectory
-from tqdm import tqdm
-from . import cfg
-from .gui import run_gui
-from .programs import Images, Upgrade as UpgradeProgram, PickOne
-from .upgrade import Upgrade
+import functools
+
+from butter.gui import run_gui
+import butter.config as config
+from butter.db import DatabaseLoader
+
+
+class DatabaseLoaderType(click.ParamType):
+    name = 'database'
+
+    def __init__(self):
+        super().__init__()
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, DatabaseLoader):
+            return value
+        try:
+            return DatabaseLoader(value)
+        except Exception as e:
+            self.fail(f"Failed to open database: '{value}': {e}")
+
+db_argument = functools.partial(
+    click.argument, type=DatabaseLoaderType(), default=config.default_database
+)
 
 
 @click.group(invoke_without_command=True)
-@click.option('--no-plugins', 'allow_plugins', default=True, flag_value=False)
-@click.option('--plugin', '-p', 'plugins', multiple=True, type=str)
 @click.pass_context
-def main(ctx, plugins, allow_plugins):
-    """Extensible image database tool."""
-    ctx.obj = cfg
-    cfg.load_plugins = allow_plugins
-    for plugin in plugins:
-        cfg.load_plugin(plugin)
+def main(ctx):
+    """Extensible image database."""
     if ctx.invoked_subcommand is None:
         ctx.invoke(gui)
 
@@ -32,24 +36,24 @@ def main(ctx, plugins, allow_plugins):
 @main.command('list')
 def list_dbs():
     """Show a list of databases."""
-    for db in cfg.databases():
+    for db in config.databases:
         print(db)
 
 
 @main.command()
-@cfg.db_argument()
-def status(db):
+@db_argument('loader')
+def status(loader):
     """Show basic information about a database."""
-    with db.database(regular=False) as db:
+    with loader.database() as db:
         print(db)
 
 
 @main.command()
-@cfg.db_argument()
-def gui(db):
+@db_argument('loader')
+def gui(loader):
     """Launch the GUI."""
-    with db.database(regular=True) as db:
-        run_gui(db=db)
+    with loader.database() as db:
+       run_gui(db=db)
 
 
 @main.command()
@@ -57,135 +61,10 @@ def gui(db):
 @click.option('--pull/--no-pull', default=True)
 @click.option('--stage/--no-stage', default=True)
 @click.option('-v', '--verbose', default=False, is_flag=True)
-@cfg.db_argument()
-def sync(db, **kwargs):
+@db_argument('loader')
+def sync(loader, **kwargs):
     """Synchronize a database."""
-    db.sync(**kwargs)
-
-
-@main.command('push-config')
-@cfg.db_argument()
-def push_config(db):
-    """Push config to remote."""
-    db.push_config()
-
-
-@main.command('show-tweaks')
-@cfg.db_argument()
-def show_tweaks(db):
-    with db.database(regular=False) as db:
-        pics = list(db.tweak_pics())
-        if pics:
-            run_gui(program=Images.factory(*pics))
-
-
-def image_hash(pic_id, pic_filename):
-    return (pic_id, imagehash.phash(Image.open(pic_filename)))
-
-def image_diff(a, b):
-    pic_a, hash_a = a
-    pic_b, hash_b = b
-    return (pic_a, pic_b, abs(hash_a - hash_b))
-
-@main.command()
-@click.option('--threshold', '-t', type=int, default=9)
-@click.option('--nprocs', type=int, default=1)
-@click.option('--chunksize', type=int, default=20)
-@cfg.db_argument()
-def deduplicate(db, threshold, nprocs, chunksize):
-    """Find duplicate images."""
-    with db.database(regular=False) as db:
-        pics = {pic.id: pic.filename for pic in db.query()}
-        pool = multiprocessing.Pool(nprocs)
-        hashes = pool.starmap(image_hash, tqdm(pics.items(), desc='Computing hashes'),
-                              chunksize=chunksize)
-        pool.close()
-
-        # Could use multiprocessing here too but it seems communication-dominated
-        ndiffs = len(hashes) * (len(hashes) - 1) // 2
-        diffs = [(pic_a, pic_b, abs(hash_a - hash_b))
-                 for (pic_a, hash_a), (pic_b, hash_b)
-                 in tqdm(combinations(hashes, 2), total=ndiffs, desc='Computing diffs')
-                 if abs(hash_a - hash_b) <= threshold]
-
-        clusters = {}
-        for id_a, id_b, diff in diffs:
-            cluster = clusters.get(id_a, set()) | clusters.get(id_b, set()) | {id_a, id_b}
-            for id in cluster:
-                clusters[id] = cluster
-        clusters = {frozenset(cluster) for cluster in clusters.values()}
-
-        p = inflect.engine()
-        print('Found {} {}'.format(len(clusters), p.plural('cluster', len(clusters))))
-        input('Press any key to continue...')
-        for cluster in clusters:
-            pics = [db.pic_by_id(id) for id in cluster]
-            run_gui(program=PickOne.factory(*pics))
-
-
-def download_url(url, path, base):
-    try:
-        _, ext = splitext(url)
-        ext = ext.split('?')[0]
-    except:
-        ext = '.jpg'
-    try:
-        response = requests.get(url, stream=True, timeout=10)
-        target = join(path, base + ext)
-        with open(target, 'wb') as out:
-            shutil.copyfileobj(response.raw, out)
-        return target
-    except:
-        pass
-
-def upgrade_pic(pic, u, number, prompt='>>> '):
-    alternatives = [pic]
-    urls = []
-    cmds = ['view', 'search', 'get', 'choose']
-    p = inflect.engine()
-    with TemporaryDirectory() as path:
-        urls = u.potential_urls(pic.filename, number)
-        print('Found {} {}'.format(len(urls), p.plural('URL', len(urls))))
-        alternatives = [pic]
-        for i, url in enumerate(urls):
-            target = download_url(url, path, str(i))
-            if target:
-                alternatives.append(target)
-        print('Have {} {}'.format(
-            len(alternatives), p.plural('alternative', len(alternatives)))
-        )
-        ret = run_gui(program=UpgradeProgram.factory(pic, *alternatives))
-        pic.db.session.commit()
-    return ret
-
-@main.command()
-@click.option('--samples', '-s', type=int, default=5)
-@cfg.db_argument()
-def tweak(db, samples):
-    with db.database(regular=False) as db, Upgrade() as u, TemporaryDirectory() as path:
-        pics = list(db.tweak_pics())
-        for i, pic in enumerate(pics):
-            run_gui(program=Images.factory(pic))
-            while True:
-                prompt = '({}/{}) >>> '.format(i+1, len(pics))
-                s = input(prompt).strip()
-                if s in {'', 'skip'}:
-                    break
-                elif s == 'done':
-                    return
-                elif s == 'view':
-                    run_gui(program=Images.factory(pic))
-                elif s in {'a', 'app', 'approve'}:
-                    pic.mark_tweak(False)
-                    break
-                elif s in {'u', 'up', 'upgrade'}:
-                    upgraded = upgrade_pic(pic, u, samples, prompt=prompt)
-                    if upgraded:
-                        pic.mark_tweak(False)
-                        break
-                elif s in {'d', 'del', 'delete'}:
-                    db.delete(pic)
-                    break
+    loader.sync(**kwargs)
 
 
 if __name__ == '__main__':
--- butter/config.py
+++ butter/config.py
@@ -1,96 +1,30 @@
-from contextlib import contextmanager
-import importlib.util
-from os.path import exists, isdir, join
-from os import listdir, makedirs
-import sys
+import os
+import os.path as path
+import xdg.BaseDirectory as xdg
+import yapsy.PluginManager as yapsy
 
-import click
-import xdg.BaseDirectory
-
-import butter.db as db
+import butter.plugin as plugin
 
 
-def ensure_dir(path):
-    if not exists(path):
-        makedirs(path)
+root_path = xdg.save_config_path('butter')
+data_path = path.join(root_path, 'databases')
+plugin_path = path.join(root_path, 'plugins')
+
+databases = sorted(os.listdir(data_path))
+try:
+    default_database = databases[0]
+except IndexError:
+    default_database = None
 
 
-class DatabaseParamType(click.ParamType):
-    name = 'database'
-
-    def __init__(self):
-        super(DatabaseParamType, self).__init__()
-
-    def convert(self, value, param, ctx):
-        if isinstance(value, db.loader_class):
-            return value
-        try:
-            cfg = ctx.find_object(MasterConfig)
-            return cfg.database_loader(value)
-        except Exception as e:
-            self.fail("Failed to open database '{}': {}".format(value, e))
-
-DATABASE_LOADER = DatabaseParamType()
-
-
-class MasterConfig:
+class PluginManager(yapsy.PluginManager):
 
     def __init__(self):
-        self.config_path = xdg.BaseDirectory.save_config_path('butter')
+        super().__init__(categories_filter={
+            'all': plugin.PluginBase,
+        })
+        self.setPluginPlaces([plugin_path])
+        self.collectPlugins()
 
-        self.plugin_path = join(self.config_path, 'plugins')
-        ensure_dir(self.plugin_path)
-        sys.path.append(self.plugin_path)
-        self.loaded_plugins = set()
-        self.load_plugins = True
-
-        self.db_path = join(self.config_path, 'databases')
-        ensure_dir(self.db_path)
-
-    def load_plugin(self, name, main=None):
-        if name in self.loaded_plugins or not self.load_plugins:
-            return
-        path = join(self.plugin_path, name)
-        if isdir(path):
-            try:
-                spec = importlib.util.spec_from_file_location(name, join(path, '__init__.py'))
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
-                if hasattr(module, 'enable'):
-                    module.enable()
-                self.loaded_plugins.add(name)
-            except Exception as e:
-                print("Failed to load plugin '{}': {}".format(name, e), file=sys.stderr)
-
-    def plugin_loaded(self, name):
-        return name in self.loaded_plugins
-
-    def databases(self):
-        return sorted(listdir(self.db_path))
-
-    def default_database_name(self):
-        dbs = self.databases()
-        if len(dbs) == 1:
-            return dbs[0]
-        return None
-
-    def database_loader(self, name):
-        assert name in self.databases()
-        loader = db.loader_class(name, join(self.db_path, name))
-        for plugin in loader.plugins:
-            self.load_plugin(plugin)
-        return db.loader_class(name, join(self.db_path, name))
-
-    @contextmanager
-    def database(self, name, *args, **kwargs):
-        with self.database_loader(name).database(*args, **kwargs) as db:
-            yield db
-            db.close()
-
-    def db_argument(self, argname='db'):
-        def decorator(fn):
-            return click.argument(argname, type=DATABASE_LOADER,
-                                  default=self.default_database_name())(fn)
-        return decorator
-
-cfg = MasterConfig()
+    def activate(self, name):
+        self.activatePluginByName(name)
